cf2c2fca6b89b52782ef89344b7ff4e18453e4b7
aumptifel <aump.tifel@gmail.com>
2024-05-09 23:08:09 +0700

    Added ytdl code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+const ytdl = require('ytdl-core')
+
 const app = express()
+var cors = require("cors");
+app.use(cors());
+
+app.get('/download', async (req, res) => {
+    try {
+        const url = req.query.url
+        const videoId = await ytdl.getURLVideoID(url)
+        const metaInfo = await ytdl.getInfo(url)
+        let data = {
+            url: 'https://www.youtube.com/embed/'+videoId,
+            info: metaInfo.formats
+        }
+        return res.send(data)
+    } catch(error) {
+        return res.status(500)
+    }
+})
 
 app.use("/", (req, res) => {
     res.send("Server is runing.");
 })
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.listen(4000, () => {
+    console.log(`Server is running on PORT: 4000`)
+})
